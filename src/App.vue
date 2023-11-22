68e8b01ec8fa1e407aa8c66ef90d6170d7cd1c21
Naza29 <125493858+Naza29@users.noreply.github.com>
2023-11-22 18:47:40 +0000

    🌟 Feat: Assets & Brazil Add assets to project and brazil route

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+   <div id="nav">
+   <router-link to="/">Inicio</router-link> 
+   <router-link to="/brazil">Brasil</router-link>
+   <router-link to="/about">Acerca de</router-link>
+   </div>
+   <div class="container">
+   <RouterView />
+   </div>
+</template>
+
+<style scoped>
+</style>
